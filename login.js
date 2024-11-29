1677ba93d42ea62ad0b1e9fd548e2636c7fe75ab
abhijith2021 <Abhijithkishoretvm20@gmail.com>
2024-11-29 23:14:13 +0530

    111

---
+++ login.js
@@ -0,0 +1,11 @@
+let names = document.getElementById('fname').value;
+
+function show(){
+    console.log(names);
+}
+
+function myFunction() {
+    var x = document.getElementById("fname").value;
+    console.log(x)
+    // document.getElementById("demo").innerHTML = "You selected: " + x;
+  }
